8ce69039825f7c9886fb745d88d228ab51cfbc7d
avadhutgiri12 <avadhutgiri123@gmail.com>
2024-12-19 15:02:57 +0530

    Add messages

--- numberguess.py
+++ numberguess.py
@@ -2,12 +2,14 @@ import random
 
 computer = random.randint(1,100)
 
-
+print('''Welcome to the Number Guessing Game!
+I'm thinking of a number between 1 and 100.
+You have 5 chances to guess the correct number.\n''')
 
 print('''Choose Difficulty Level:
       1.Easy (10 Chances)
       2.Medium (5 Chances)
-      3.Hard (3 Chances)''')
+      3.Hard (3 Chances)\n''')
 
 level = int(input("Enter Your choice: "))
 
@@ -27,12 +29,15 @@ def game():
 
 def chance():
        if (level == 1):
+              print("Great! You have selected the easy difficulty level.\nLet's start the game!")
               for i in range(1,11):
                      game()
        if (level == 2):
+              print("Great! You have selected the Medium difficulty level.\nLet's start the game!")
               for i in range(1,6):
                      game()
        if (level == 3):
+              print("Great! You have selected the Medium difficulty level.\nLet's start the game!")
               for i in range(1,4):
                      game()
 
